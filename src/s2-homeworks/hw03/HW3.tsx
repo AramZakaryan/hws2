5d8e2a65602139006dd86bc6b9d5223b88453e17
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-09-07 22:58:57 +0400

    Interim Result of HomeWork 3

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -4,9 +4,9 @@ import s2 from '../../s1-main/App.module.css'
 import GreetingContainer from './GreetingContainer'
 
 /*
-* 1 - описать тип UserType
-* 2 - указать нужный тип в useState с users
-* 3 - дописать типы и логику функции pureAddUserCallback и проверить её тестами
+* + 1 - описать тип UserType
+* + 2 - указать нужный тип в useState с users
+* + 3 - дописать типы и логику функции pureAddUserCallback и проверить её тестами
 * 4 - в файле GreetingContainer.tsx дописать типизацию пропсов
 * 5 - в файле GreetingContainer.tsx указать нужные типы в useState с name и error
 * 6 - в файле GreetingContainer.tsx дописать тип и логику функции setNameCallback
@@ -19,18 +19,20 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // + need to fix any
+    name: string // + need to fix any
 }
 
 export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
-    const user = { // need to fix
+    const user:UserType = { // + need to fix
+        _id: v1(),
+        name: name
     }
     setUsers([...users, user])
 }
 
 const HW3 = () => {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<UserType[]>([]) // + need to fix any
 
     const addUserCallback = (name: any) => { // need to fix any
         pureAddUserCallback(name, setUsers, users)
