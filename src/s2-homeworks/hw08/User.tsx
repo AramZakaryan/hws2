3866bcb7c3db0a5a0f215f57c3ac64e13ce99df2
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-01-27 16:58:51 +0400

    Final Result of HomeWork 8

--- src/s2-homeworks/hw08/User.tsx
+++ src/s2-homeworks/hw08/User.tsx
@@ -11,11 +11,13 @@ const User: React.FC<UserPropsType> = ({ u }) => {
     return (
         <tr id={'hw8-user-' + u._id + '-' + u.age} className={s.item}>
             <td id={'hw8-user-name-' + u._id} className={s.nameCol}>
-                {/*отобразить имя*/}
+                {/* + отобразить имя*/}
+                {u.name}
 
             </td>
             <td id={'hw8-user-age-' + u._id}>
-                {/*отобразить возраст*/}
+                {/* + отобразить возраст*/}
+                {u.age}
 
             </td>
         </tr>
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,39 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+// export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // + need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            // return state // need to fix
+            let stateCopy=[...state]
+            if (action.payload === "up") {
+                return stateCopy.sort((a, b) => {
+                        if (a.name < b.name) {
+                            return -1
+                        }
+                        if (a.name > b.name) {
+                            return 1
+                        }
+                        return 0
+                    }
+                )
+            } else {
+                return stateCopy.sort((a, b) => {
+                        if (a.name < b.name) {
+                            return 1
+                        }
+                        if (a.name > b.name) {
+                            return -1
+                        }
+                        return 0
+                    }
+                )
+            }
         }
         case 'check': {
-
-            return state // need to fix
+            // return state // + need to fix
+            return state.filter(u => u.age >= action.payload)
         }
         default:
             return state
