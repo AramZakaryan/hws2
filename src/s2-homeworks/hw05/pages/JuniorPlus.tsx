9ca38c8872ac5309f64a698f72d89e613db8ebe7
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-03-13 22:21:58 +0400

    Interim Result of HomeWork 11

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HW10 from "../../hw10/HW10";
 import HW11 from "../../hw11/HW11";
+import HW12 from "../../hw12/HW12";
 // import HW10 from '../../hw10/HW10'
 // import HW11 from '../../hw11/HW11'
 // import HW12 from '../../hw12/HW12'
@@ -12,8 +13,8 @@ function JuniorPlus() {
     return (
         <div id={'hw5-page-junior-plus'}>
             {/*<HW10 />*/}
-            <HW11 />
-            {/*<HW12 />*/}
+            {/*<HW11 />*/}
+            <HW12 />
             {/*<HW13 />*/}
             {/*<HW14 />*/}
             {/*<HW15 />*/}
